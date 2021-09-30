5ef03dd54a6eb92776b7ece690f1ce0e8bdc3f2f
Akshaya <bhattaraiakshaya@gmail.com>
2021-09-29 22:30:44 -0400

    create constants.py

---
+++ scripts/constants.py
@@ -0,0 +1,43 @@
+
+CHROME_DRIVER_EXECUTABLE_PATH='/Users/akshayabhattarai/Documents/github/SeeMe/env/chromedriver'
+
+# scrap_daraz_mobile.py
+product_card_tag_type = "div"
+product_card_class_name = "c3KeDq"
+
+product_name_tag_type = "div"
+product_name_class_name = "c16H9d"
+
+product_price_tag_type = "span"
+product_price_class_name = "c13VH6"
+
+og_price_tag_type = "del"
+og_price_class_name = "c13VH6"
+
+discount_percent_tag_type = "span"
+discount_percent_class_name = "c1hkC1"
+
+review_count_tag_type = "span"
+review_count_class_name = "c3XbGJ"
+
+
+DARAZ_DRIVER_TIMEOUT = 45
+
+# daraz_category_urls.py
+DARAZ_CATEGORY_ROOT_MENU_CLASSNAME = 'lzd-site-menu-root-item'
+DARAZ_CATEGORY_ROOT_MENU_TAG = 'li'
+
+DARAZ_CATEGORY_SUB_MENU_CLASSNAME = 'lzd-site-menu-sub'
+DARAZ_CATEGORY_SUB_MENU_TAG = 'ul'
+
+DARAZ_CATEGORY_SUB_MENU_ITEM = 'lzd-site-menu-sub-item'
+DARAZ_CATEGORY_SUB_MENU_ITEM_TAG = 'li'
+
+DARAZ_CATEGORY_L2_TABLE = 'lzd-site-menu-grand'
+DARAZ_CATEGORY_L2_TABLE_TAG = 'ul'
+
+DARAZ_CATEGORY_L2_ITEM = 'lzd-site-menu-grand-item'
+DARAZ_CATEGORY_L2_ITEM_TAG = 'li'
+
+DARAZ_CATEGORY_L3_CLASSNAME = 'lzd-site-menu-grand-item'
+DARAZ_CATEGORY_L3_MENU_TAG = 'li'
